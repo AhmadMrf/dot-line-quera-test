11e6d61091c25fee2a6763da4bc701f3e826c216
ahmad-mrf <naashna20@gmail.com>
2022-07-15 10:24:02 +0430

    complete game logic

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const N = 4;
 const M = 4;
 
+const maxLineCount = ((lenght, width) => {
+  let lineCount = 0;
+  if (lenght === width) {
+    lineCount = lenght * (width - 1) * 2;
+    return lineCount;
+  }
+
+  let less = Math.min(lenght, width);
+  let difference = Math.abs(lenght - width);
+
+  lineCount = less * (less - 1) * 2 + (less + (less - 1)) * difference;
+  return lineCount;
+})(M, N);
+
 let turn = "R";
 let selectedLines = [];
 
+const scors = { R: 0, B: 0 };
 const hoverClasses = { R: "hover-red", B: "hover-blue" };
 const bgClasses = { R: "bg-red", B: "bg-blue" };
 
@@ -29,8 +44,6 @@ const createGameGrid = () => {
       const hLine = document.createElement("div");
       hLine.setAttribute("class", `line-horizontal ${hoverClasses[turn]}`);
       hLine.setAttribute("id", `h-${row}-${col}`);
-      hLine.innerHTML = `h-${row}-${col}`;
-
       hLine.addEventListener("click", handleLineClick);
 
       gameGridContainer.appendChild(dot);
@@ -42,14 +55,11 @@ const createGameGrid = () => {
         const vLine = document.createElement("div");
         vLine.setAttribute("class", `line-vertical ${hoverClasses[turn]}`);
         vLine.setAttribute("id", `v-${row}-${col}`);
-        vLine.innerHTML = `v-${row}-${col}`;
-
         vLine.addEventListener("click", handleLineClick);
 
         const box = document.createElement("div");
         box.setAttribute("class", "box");
         box.setAttribute("id", `box-${row}-${col}`);
-        box.innerHTML = `box-${row}-${col}`;
         gameGridContainer.appendChild(vLine);
         if (col < M - 1) gameGridContainer.appendChild(box);
       });
@@ -63,7 +73,6 @@ const changeTurn = () => {
   const nextTurn = turn === "R" ? "B" : "R";
 
   const lines = document.querySelectorAll(".line-vertical, .line-horizontal");
-
   lines.forEach((l) => {
     //if line was not already selected, change it's hover color according to the next turn
     if (!isLineSelected(l)) {
@@ -75,26 +84,32 @@ const changeTurn = () => {
 
 const handleLineClick = (e) => {
   const lineId = e.target.id;
-  isCompleted(lineId);
   const selectedLine = document.getElementById(lineId);
-
   if (isLineSelected(selectedLine)) {
     //if line was already selected, return
     return;
   }
 
   selectedLines = [...selectedLines, lineId];
-  console.log(selectedLines);
+
+  let selectedBox = witchBoxesSelected(lineId);
+  if (selectedBox) {
+    selectedBox.forEach((selectedBox) => {
+      selectedBox.classList.add(bgClasses[turn]);
+    });
+    scors[turn] += selectedBox.length;
+    colorLine(selectedLine);
+    if (maxLineCount == selectedLines.length) {
+      let won = scors.R > scors.B ? "Red" : "Blue";
+      document.getElementById("game-status").innerHTML = `won ${won} - Blue : ${scors.B} / Red : ${scors.R}`;
+      return;
+    }
+    return;
+  }
 
   colorLine(selectedLine);
   changeTurn();
   document.getElementById("game-status").innerHTML = playersTurnText(turn);
-};
-
-const isCompleted = (lineId) => {
-  if (/(v)/i.test(lineId)) console.log("v", lineId);
-  if (/(h)/i.test(lineId)) console.log("h", lineId);
-  return true;
 };
 
 const colorLine = (selectedLine) => {
@@ -102,4 +117,30 @@ const colorLine = (selectedLine) => {
   selectedLine.classList.add(bgClasses[turn]);
 };
 
+// return null or selected box element
+const witchBoxesSelected = (lineId) => {
+  let [direction, row, col] = lineId.split("-");
+  let boxes = null;
+  if (direction === "v") {
+    boxes = [`box-${row}-${col - 1}`, `box-${row}-${col}`];
+  } else {
+    boxes = [`box-${row - 1}-${col}`, `box-${row}-${col}`];
+  }
+
+  let completedBoxId = null;
+  let completedBoxes = null;
+  let completedBoxesId = boxes.filter((box) => {
+    let [, rowBox, colBox] = box.split("-");
+
+    let rigthLine = `v-${rowBox}-${+colBox + 1}`;
+    let leftLine = `v-${rowBox}-${colBox}`;
+    let topLine = `h-${rowBox}-${colBox}`;
+    let bottonLine = `h-${+rowBox + 1}-${colBox}`;
+
+    let boxLines = [rigthLine, leftLine, topLine, bottonLine];
+    completedBoxId = boxLines.every((line) => selectedLines.includes(line));
+    return completedBoxId;
+  });
+  return completedBoxesId.length ? completedBoxesId.map((boxId) => document.getElementById(boxId)) : null;
+};
 createGameGrid();
